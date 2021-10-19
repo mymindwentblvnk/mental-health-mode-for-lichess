77289d3ea209e3f71c051037837c9daffc226b73
Michael Eichenseer <michael.eichenseer@codecentric.de>
2021-10-19 20:47:45 +0200

    Activate toggle for Mental Health Mode.

--- src/mental-health-mode.js
+++ src/mental-health-mode.js
@@ -63,6 +63,6 @@ function applyMentalHealthMode() {
 }
 
 // Only apply when Mental Health Mode is activated
-if (localStorage.getItem("mhm.activated") == "yes") {
+if (localStorage.getItem("mhm.activated") != "no") {
     applyMentalHealthMode();
 }
--- src/preferences.js
+++ src/preferences.js
@@ -18,16 +18,36 @@ const MHM_ACTIVATED = "mhm.activated";
 const YES = "yes";
 const NO = "no";
 
-console.log("Mental Health Mode in localStorage activated: ", localStorage.getItem(MHM_ACTIVATED))
-// Not yet set OR set to active
-if (localStorage.getItem(MHM_ACTIVATED) == null || localStorage.getItem(MHM_ACTIVATED) == YES) {
+function activateMentalHealthMode() {
     localStorage.setItem(MHM_ACTIVATED, YES);
     document.getElementById("irdisplay_mhm_0").removeAttribute("checked");
     document.getElementById("irdisplay_mhm_1").setAttribute("checked", "checked");
-// Set to not active
-} else if(localStorage.getItem(MHM_ACTIVATED) == NO) {
+}
+
+function deactivateMentalHealthMode() {
+    localStorage.setItem(MHM_ACTIVATED, NO);
     document.getElementById("irdisplay_mhm_0").setAttribute("checked", "checked");
     document.getElementById("irdisplay_mhm_1").removeAttribute("checked");
+}
+
+console.log("Mental Health Mode in localStorage activated: ", localStorage.getItem(MHM_ACTIVATED))
+// Not yet set OR set to active
+if (localStorage.getItem(MHM_ACTIVATED) == null || localStorage.getItem(MHM_ACTIVATED) == YES) {
+    activateMentalHealthMode();
+// Set to not active
+} else if(localStorage.getItem(MHM_ACTIVATED) == NO) {
+    deactivateMentalHealthMode();
 } else {
     alert("This should never show up. Congrats.");
 }
+
+// Apply onChange listener
+document.getElementById("irdisplay_mhm_0").addEventListener('change', function(evt) {
+    deactivateMentalHealthMode();
+    location.reload();
+}, false);
+
+document.getElementById("irdisplay_mhm_1").addEventListener('change', function(evt) {
+    activateMentalHealthMode();
+    location.reload();
+}, false);
